a7b73eea53aa9e531ac1da6eb2a20d2438d9defa
OverlayCS <overlay720@outlook.com>
2025-03-23 18:02:19 +0000

    Add files via upload
    Source Code Added

---
+++ PlayFabPlus/PlayFabPlusUtils.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using PlayFab.ClientModels;
+using PlayFab;
+using System;
+using PlayFab.PlayFabPlus;
+
+namespace Assets.PlayFabPlus
+{
+	public class CoinService
+	{
+		public int GetVirtualCurreny(Action<int> OnGetVirtualCurrenySuccess, Action<int> OnGetVirtualCurrenyFailed)
+        {
+			int coins = 0;
+
+            PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(), OnGetUserInventorySuccess =>
+			{
+                coins = OnGetUserInventorySuccess.VirtualCurrency[PlayFabPlusUtils.GetCurrencyCode()];
+                OnGetVirtualCurrenySuccess.Invoke(coins);
+            }, OnError =>
+			{
+				OnGetVirtualCurrenyFailed.Invoke(0);
+            });
+
+			return 0;
+        }
+    }
+
+	public static class PlayFabPlusUtils
+	{
+		public static string GetCurrencyCode()
+		{
+            return PlayFabPlusCore.CurrenyCode;
+        }
+	}
+}
